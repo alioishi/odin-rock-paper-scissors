38f0f295768957570e8f6f37da7cf54a9a413453
alioishi <29942094+alioishi@users.noreply.github.com>
2022-05-21 12:17:07 -0700

    Add round play

--- main.js
+++ main.js
@@ -15,6 +15,10 @@ let ROCK = 0;
 let PAPER = 1;
 let SCISSORS = 2;
 
+let USER_WIN = 0;
+let COMPUTER_WIN = 1;
+let DRAW = 2;
+
 function convertMoveStrToMoveInt(move) {
     return (move == "rock") ? ROCK :
         (move == "paper") ? PAPER : 
@@ -34,4 +38,19 @@ function getUserMove() {
 
 function computerPlay() {
     return computerMoveRNG = Math.floor(Math.random()*3);
+}
+
+function playRound (userSelection, computerSelection) {
+    if (userSelection == computerSelection){
+        return DRAW;
+    }
+    else if (userSelection == SCISSORS){
+        return (computerSelection == ROCK) ? COMPUTER_WIN : USER_WIN;
+    }
+    else if (computerSelection == SCISSORS){
+        return (userSelection == ROCK) ? USER_WIN : COMPUTER_WIN;
+    }
+    else {
+        return (userSelection > computerSelection) ? USER_WIN : COMPUTER_WIN;
+    }
 }
