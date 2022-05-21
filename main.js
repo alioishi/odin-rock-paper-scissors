8e66fa93b419e1d74e8667df2a2e89da03ea2fae
alioishi <29942094+alioishi@users.noreply.github.com>
2022-05-21 11:55:28 -0700

    Add function to convert moves from strings to integers

--- main.js
+++ main.js
@@ -6,25 +6,32 @@ for 5 rounds
     generate random move
     compare moves to determine round winner or draw
 output winner as best 3 out of 5 unless there is a draw
+sudden death
 */
 
 // all returns in lowercase for consistency
 
-function getUserMove (){
+let ROCK = 0;
+let PAPER = 1;
+let SCISSORS = 2;
+
+function convertMoveStrToMoveInt(move) {
+    return (move == "rock") ? ROCK :
+        (move == "paper") ? PAPER : 
+        SCISSORS;
+}
+
+function getUserMove() {
     let userMove = prompt("Rock, Paper, Scissors?");
     let userMoveCheck = userMove.toLowerCase();
     if (userMoveCheck == "rock" || userMoveCheck == "paper" || userMoveCheck == "scissors"){
-        return userMoveCheck;
+        return convertMoveStrToMoveInt(userMoveCheck);
     }
     else{
         console.log(`${userMove} is not valid.` );
     }
 }
 
-function computerPlay(){
-    let computerMoveRNG = Math.floor(Math.random()*3);
-  
-    return (computerMoveRNG == 0) ? "rock" : 
-    (computerMoveRNG == 1) ? "paper" : 
-    "scissors";
+function computerPlay() {
+    return computerMoveRNG = Math.floor(Math.random()*3);
 }
